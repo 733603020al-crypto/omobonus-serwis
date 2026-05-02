2fff9e926421cbf7b240c9aefcedb4ea5057d181
733603020al-crypto <733603020al@gmail.com>
2026-05-02 13:42:57 +0200

    Opinie klientów z Google с файла

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion"
+
 export default function Loader() {
     return (
         <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-black">
